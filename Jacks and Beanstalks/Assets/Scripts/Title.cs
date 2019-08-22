da0f1de8df653e1ed7a909657151e5ce2373802e
aycaramba <rjsxkr1122@naver.com>
2019-08-22 18:10:38 +0900

    타이틀 씬 만듬

---
+++ Jacks and Beanstalks/Assets/Scripts/Title.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public GameObject titleImaga;
+
+    Sprite on;
+
+    Sprite off;
+
+    bool isOn = true;
+
+    void Start()
+    {
+        on = titleImaga.GetComponent<SpriteRenderer>().sprite;
+
+        off = Resources.Load<Sprite>("Images/Title_OFF");
+
+        StartCoroutine(Blink());
+    }
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+    IEnumerator Blink()
+    {
+        if (isOn)
+        {
+            titleImaga.GetComponent<SpriteRenderer>().sprite = off;
+
+            isOn = false;
+        }
+        else
+        {
+            titleImaga.GetComponent<SpriteRenderer>().sprite = on;
+
+            isOn = true;
+        }
+
+        yield return new WaitForSeconds(1.0f);
+
+        StartCoroutine(Blink());
+    }
+}
