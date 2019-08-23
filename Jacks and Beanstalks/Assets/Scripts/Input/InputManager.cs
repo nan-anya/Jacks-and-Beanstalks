1f40c6711f300c52729272b98f26dac5c4adb652
kimdhmath <41560246+kimdhmath@users.noreply.github.com>
2019-08-23 15:35:54 +0900

    input스크립트 생성InputTest씬에 저장

---
+++ Jacks and Beanstalks/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,91 @@
+﻿
+public class InputManager
+{
+    private int growth_count;
+    private bool smile_sun, sad_sun, rain, fog, lightning, bomb;
+    private int L_count, R_count, Up_count;
+
+    public InputManager()
+    {
+        growth_count = 0;
+
+        smile_sun = false;
+        sad_sun = false;
+        rain = false;
+        fog = false;
+        lightning = false;
+        bomb = false;
+
+        L_count = 0;
+        R_count = 0;
+        Up_count = 0;
+    }
+    
+
+    public void PlusGrowth()//성장변수 1씩증가
+    {
+        growth_count++;
+    }
+    public void ResetGrowth()//성장변수 0으로 리셋
+    {
+        growth_count = 0;
+    }
+    public int GetGrowth()//성장변수 값알려주기
+    {
+        return growth_count;
+    }
+    
+
+    public void PlusUpCount()//1회 올라가기
+    {
+        Up_count++;
+    }
+    public int GetUpCount()//몇번 올라갔는지
+    {
+        return Up_count;
+    }
+
+
+    public int GetLeftCount()//왼쪽 몇회
+    {
+         return L_count;
+        
+    }
+    public void PlusLeftCount()//왼쪽 1회 추가
+    {
+        L_count++;
+    }
+    public void ResetLeftCount()//왼쪽 reset
+    {
+        L_count = 0;
+    }
+
+
+    public int GetRightCount()//오른쪽 몇회
+    {
+        return R_count;
+    }
+    public void PlusRightCount()//오른쪽 1회 추가
+    {
+        R_count++;
+    }
+    public void ResetRightCount()//오른쪽 reset
+    {
+        R_count = 0;
+    }
+
+    
+    public void Up()//성장하면 왼쪽과 오른쪽 카운트 0
+    {
+        L_count = 0;
+        R_count = 0;
+    }
+
+
+    public bool Smile_Sun { get { return smile_sun; } set { smile_sun = Smile_Sun; } }//웃는 해
+    public bool Sad_Sun { get { return sad_sun; } set { sad_sun = Sad_Sun; } }//찡그린 해
+    public bool Rain { get { return rain; } set { rain = Rain; } }//비구름
+    public bool Fog { get { return fog; } set { fog = Fog; } }//안개
+    public bool Lightning { get { return lightning; } set { lightning = Lightning; } }//번개
+    public bool Bomb { get { return bomb; } set { bomb = Bomb; } }//폭탄
+}
---
+++ Jacks and Beanstalks/Assets/Scripts/Input/LPlayerInput.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LPlayerInput : MonoBehaviour
+{
+    private int count;
+    public GameObject Smile_sun_on, Sad_sun_on, Rain_on, Jack_left, Jack_right;
+    public InputManager inputmanager = new InputManager();
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+        Jack_left.GetComponent<SpriteRenderer>().enabled = false;
+        Jack_right.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))//왼쪽물주기
+        {
+            timer += Time.deltaTime;
+            inputmanager.PlusLeftCount();
+            Jack_left.GetComponent<SpriteRenderer>().enabled = true;
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.E) )//오른쪽물주기
+        {
+            inputmanager.PlusRightCount();
+            Jack_right.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))//성장시키기
+        {
+            inputmanager.PlusGrowth();
+        }
+
+        if(timer > 0.3f)
+        {
+            Jack_left.GetComponent<SpriteRenderer>().enabled = false;
+            Jack_right.GetComponent<SpriteRenderer>().enabled = false;
+            timer = 0.0f;
+        }
+        else if(timer <= 0.3f)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            timer = 0.0f;
+            Jack_left.GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            timer = 0.0f;
+            Jack_right.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        
+        
+        Smile_sun_on.GetComponent<SpriteRenderer>().enabled = inputmanager.Smile_Sun;
+        Sad_sun_on.GetComponent<SpriteRenderer>().enabled = inputmanager.Sad_Sun;
+        Rain_on.GetComponent<SpriteRenderer>().enabled = inputmanager.Rain;
+    }
+}
